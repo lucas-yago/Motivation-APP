6b5c2251bc5fd7a27b689ef1acd53db07cd2fb17
Lucas Yago <lucasyago63@gmail.com>
2022-07-21 12:39:28 -0300

    adiciona evendo de click ao botao de nova frase

--- app/src/main/java/com/lucasyago/motivation/MainActivity.kt
+++ app/src/main/java/com/lucasyago/motivation/MainActivity.kt
@@ -2,10 +2,28 @@ package com.lucasyago.motivation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import com.lucasyago.motivation.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonNewPhrase.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        if (view.id == R.id.button_newPhrase){
+            newPhrase()
+        }
+    }
+
+    private fun newPhrase() {
+        Toast.makeText(this, "voce solicitou uma nova frase", Toast.LENGTH_SHORT).show()
     }
 }
